bfcde28b6db060be1278a670a2965074628106fc
Pamella Hani <pam02hani@gmail.com>
2023-11-17 11:02:38 +0100

    added interface + class ProdCons completed but main not done

--- ProdConsBuffer.java
+++ ProdConsBuffer.java
@@ -1,25 +1,67 @@
 package ProjetPC;
-public class ProdConsBuffer {
-    public interface IProdConsBuffer {
-        /**
-        * Put the message m in the buffer
-        **/
-        public void put(Message m) throws InterruptedException;
-        /**
-        * Retrieve a message from the buffer,
-        * following a FIFO order (if M1 was put before M2, M1
-        * is retrieved before M2)
-        **
-        public Message get() throws InterruptedException;
-        /**
-        * Returns the number of messages currently available in
-        * the buffer
-        **/
-        public int nmsg();
-        /**
-        * Returns the total number of messages that have
-        * been put in the buffer since its creation
-        **/
-        public int totmsg();
+
+/*
+
++----------------+------------+------------------------+----------------+--+
+| Methode        | Pre-action | Garde                  | Post-action    |  |
++----------------+------------+------------------------+----------------+--+
+| put(Message M) | -          | nfree > 0              | nfree --       |  |
+|                |            |                        |                |  |
+|                |            | num_stored == bufferSz | num_stored ++  |  |
++----------------+------------+------------------------+----------------+--+
+| get()          | -          | num_stored = 0         | nfree ++       |  |
+|                |            |                        |                |  |
+|                |            |                        | num_stored --  |  |
++----------------+------------+------------------------+----------------+--+
+
+ */
+
+public class ProdConsBuffer implements IProdConsBuffer{
+    private int buffersize;
+    private Message[] buffer;
+    private int nfree; 
+    private int nmsg;  //=num_stored
+    private int totmsg ;; 
+
+
+
+    public ProdConsBuffer(int size){
+        buffersize = size;
+        buffer = new Message[size];
+        nfree = size ; 
+        nmsg = 0;
+        totmsg = 0; 
     }
+
+    //put a message into the buffer
+    public void put(Message m) throws InterruptedException{
+        while(nfree == 0){
+            wait();
+        }
+        buffer[nfree] = m;
+        nfree--;
+        nmsg++;
+        totmsg++;
+        notifyAll();
+    }
+
+    public Message get() throws InterruptedException{
+        while(nfree == buffersize){
+            wait();
+        }
+        Message m = buffer[nfree];
+        nfree++;
+        nmsg--;
+        notifyAll();
+        return m;
+    }
+
+    public int nmsg(){
+        return nmsg;
+    }   
+
+    public int totmsg(){
+        return totmsg;
+    }
+
 }
